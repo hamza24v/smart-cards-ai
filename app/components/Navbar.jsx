ae9facc9911c1971fe26db6294f9b730dc713ddf
hamza24v <107317017+hamza24v@users.noreply.github.com>
2024-08-16 12:50:07 -0400

    added clerk sign in/sign up

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,16 +1,26 @@
+"use client"
 import React from 'react'
-import Image from 'next/image'
-import { Avatar } from '@mui/material'
+import { Button } from '@mui/material'
+import { useRouter } from 'next/navigation'
 
 function Navbar() {
+  const router = useRouter();
+  const handleSignUp = () => {
+    router.push('/sign-up'); 
+  };
+
+  const handleSignIn = () => {
+    router.push('/sign-in');
+  };
+
     return (
       <div className='absolute top-0 w-full font-serif bg-green-50 shadow-sm p-5'>
         <div className='flex justify-between hover:space-x-30 transition-all duration-500 items-center mx-4'>
           <p className='text-2xl cursor-pointer'>SmartCards AI</p>
           <div className='flex items-center space-x-2 '>
-            <button href="#" className='text-xl hover:bg-green-100 rounded-lg p-2'>Sign Up</button>
+            <Button onClick={handleSignUp} className='text-lg text-black hover:bg-green-100 rounded-lg '>Sign Up</Button>
             <span>|</span>
-            <button href="#" className='text-xl  hover:bg-green-100 rounded-lg p-2'>Login</button>
+            <Button onClick={handleSignIn} className='text-lg text-black  hover:bg-green-100 rounded-lg '>Login</Button>
           </div>
         </div>
       </div>
