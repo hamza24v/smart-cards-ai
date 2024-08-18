e6e4a7170a52232613b64afb67a49d533bb4e6ec
hamza24v <107317017+hamza24v@users.noreply.github.com>
2024-08-18 18:24:11 -0400

    features and hero section complete

--- app/components/Features.jsx
+++ app/components/Features.jsx
@@ -4,18 +4,20 @@ import Image from "next/image";
 
 function Features() {
   return (
-    <section className="bg-gray-100 w-full py-20">
-      <h2 className="text-4xl font-bold text-center mb-10">Features</h2>
-      <div className="flex flex-row gap-4 justify-center">
-        {features.map((feature) => (
-          <div className="flex p-4 flex-col text-center justify-center  bg-gray-100 shadow-lg rounded-lg transition ease-in-out delay-150 hover:scale-105 hover:-translate-y-1">
-            <Image src={feature.image} alt="image" width={50} height={50} />
-            <p className="font-bold text-xl mb-5">{feature.title}</p>
-            <p>{feature.description}</p>
+    <section className="bg-gray-50 w-full py-20">
+    <h2 className="text-4xl font-bold text-center mb-10">Features</h2>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-6">
+      {features.map((feature) => (
+        <div className="flex flex-col items-center text-center bg-white shadow-lg rounded-lg p-6 hover:-translate-y-3 transition duration-300">
+          <div className="flex justify-center items-center  w-64 mb-4">
+            <Image src={feature.image} alt="image" width={100} height={100} className="object-contain scale-125" />
           </div>
-        ))}
-      </div>
-    </section>
+          <p className="font-bold text-xl mb-2">{feature.title}</p>
+          <p className="text-gray-600">{feature.description}</p>
+        </div>
+      ))}
+    </div>
+  </section>
   );
 }
 
--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -2,16 +2,14 @@ import React from "react";
 
 function Hero() {
   return (
-    <section className="py-20 text-center">
+    <section className="py-20 text-center items-center">
       <h1 className="text-8xl font-serif font-bold">
         SmartCards AI
       </h1>
-      <p className=" max-w-xl flex justify-center text-gray-600 leading-8">
+      <p className="max-w-3xl flex text-lg mt-5 justify-center text-gray-600 leading-8">
         Transform your study sessions with AI-powered flashcards tailored just
         for you. SmartCards AI adapts to your learning needs, helping you focus
-        on what matters most and retain knowledge more effectively. Say goodbye
-        to generic study tools—embrace personalized, efficient learning and
-        achieve your academic goals with ease.
+        on what matters most and retain knowledge more effectively. 
       </p>
     </section>
   );
