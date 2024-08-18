dc1369ead91b7c8d1cdb1ce6a63068ba4b1bd82b
hamza24v <107317017+hamza24v@users.noreply.github.com>
2024-08-18 07:57:19 -0400

    further enhancements

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -13,9 +13,9 @@ function Navbar() {
   const { isSignedIn } = useUser();
 
   return (
-    <div className="absolute top-0 w-full font-serif bg-green-50 shadow-sm p-5">
+    <div className="absolute top-0 w-full font-serif bg-green-50 shadow-lg p-5">
       <div className="flex justify-between hover:space-x-30 transition-all duration-500 items-center mx-4">
-        <p className="text-2xl cursor-pointer">SmartCards AI</p>
+        <p className="text-2xl font-semibold cursor-pointer">SmartCards AI</p>
         <div className="flex items-center space-x-2 ">
           {isSignedIn ? (
             <SignedIn>
@@ -23,13 +23,12 @@ function Navbar() {
             </SignedIn>
           ) : (
             <>
-              <div className="hover:bg-green-100 rounded-lg p-2">
+              <div className="hover:bg-green-100 font-serif text-lg rounded-lg p-2">
                 <SignedOut>
                   <SignInButton />
                 </SignedOut>
               </div>
-              <span> | </span>
-              <div className="hover:bg-green-100 rounded-lg p-2">
+              <div className="bg-gray-500 font-serif text-lg text-white hover:bg-gray-600 rounded-lg p-2">
                 <SignedOut>
                   <SignUpButton />
                 </SignedOut>
